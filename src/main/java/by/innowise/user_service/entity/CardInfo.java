fae4c2faf888978696161e968ac7084a14c10b43
KsushaTarasova <ksutaras543@mail.ru>
2025-06-13 17:46:16 +0300

    create entities

---
+++ src/main/java/by/innowise/user_service/entity/CardInfo.java
@@ -0,0 +1,43 @@
+package by.innowise.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "card_info")
+public class CardInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "number", length = 16, unique = true, nullable = false)
+    private String number;
+
+    @Column(name = "holder", length = 50, nullable = false)
+    private String holder;
+
+    @Column(name = "expiration_date", nullable = false)
+    private LocalDate expirationDate;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
